8f4d02588753856c2aa676b12c50eb7ca5f084a5
Igor Kozlov <www.bk.ru@mail.ru>
2018-04-25 22:13:26 +0300

    Add constants file.

---
+++ app/src/main/java/bobrchess/of/by/belaruschess/util/Constants.java
@@ -0,0 +1,18 @@
+package bobrchess.of.by.belaruschess.util;
+
+/**
+ * Created by Igor on 04.04.2018.
+ */
+
+public class Constants {
+    public static class URL {
+        public static final String HOST = "kek";
+       // public static final String HOST = "http://192.168.0.104:8080";
+        public static final String GET_COUNTRIES = HOST + "country";
+        public static final String GET_COUNTRY = HOST + "country/1";
+        public static final String GET_USERS = HOST + "user";
+        public static final String GET_USER = HOST + "user/1";
+        public static final String USER_REGISTRATION = HOST + "user/1";
+        public static final String USER_AUTHORIZATION = HOST + "user/1";
+    }
+}
