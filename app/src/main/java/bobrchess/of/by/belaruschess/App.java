e134883db214a2d2102a421a23a0ec06718256ed
Ihar Kazlou <Ihar_Kazlou@epam.com>
2020-12-06 16:31:49 +0300

    Temporaty certificate fix.

--- app/src/main/java/bobrchess/of/by/belaruschess/App.java
+++ app/src/main/java/bobrchess/of/by/belaruschess/App.java
@@ -2,7 +2,14 @@ package bobrchess.of.by.belaruschess;
 
 import android.app.Application;
 
-import java.io.ByteArrayInputStream;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import bobrchess.of.by.belaruschess.network.api.ExternalFideServerApi;
 import bobrchess.of.by.belaruschess.network.api.PersonalServerApi;
@@ -43,9 +50,45 @@ public class App extends Application {
         retrofitExternalFideServerApi = new Retrofit.Builder()
                 .baseUrl(Constants.Companion.getEXTERNAL_FIDE_API_SERVER_HOST())
                 .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
+                .client(getUnsafeOkHttpClient().build())
                 .build();
         externalFideServerApi = retrofitExternalFideServerApi.create(ExternalFideServerApi.class);
+    }
+
+    public static OkHttpClient.Builder getUnsafeOkHttpClient() {//todo should be fixed https://stackoverflow.com/questions/2642777/trusting-all-certificates-using-httpclient-over-https https://futurestud.io/tutorials/retrofit-2-how-to-trust-unsafe-ssl-certificates-self-signed-expired
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
+            builder.hostnameVerifier((hostname, session) -> true);
+            return builder;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static PersonalServerApi getPersonalServerApi() {
